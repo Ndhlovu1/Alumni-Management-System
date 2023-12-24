7fb396fb0e8195440bd79d375a07c14bb2545596
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-12-24 20:28:15 +0200

    Updated : Build.sh file with installation instructions

--- AMSProject/build.sh
+++ AMSProject/build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+#pip install -r requirements.txt
+pip install --upgrade pip
+
+pip install pipenv
+
+pipenv install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
